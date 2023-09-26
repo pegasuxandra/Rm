4d615ed52571bb9a2d51ea29efae11c8dc398a51
pegasuxandra <145722667+pegasuxandra@users.noreply.github.com>
2023-09-26 10:16:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+function Leer() {
+    const peli = document.getElementById("input").location;
+    //obtain an apikey on this web
+    //http://www.omdbapi.com/apikey.aspx
+    //const key='3d98d262-116f-413d-91d5-d2d176ef84f7';
+    const api_url=`https://rickandmortyapi.com/api/character`
+    buscar3(api_url);
+}
+
+function buscar1(api_url){
+
+    fetch(api_url)
+      .then(data => {
+        return data.json()
+      }).then(resultado=>{
+            console.log(resultado);
+
+            const {Search=[]} = resultado;
+            
+            console.log(Search);
+            document.getElementById("lista").innerHTML='';
+
+            Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                         <img width='100%' src=${p.Poster} alt="No hay poster"></img>
+            </div>`;
+            })
+      });
+
+
+}
+
+const buscar2=async(api_url)=>{
+
+    const data= await fetch(api_url);
+    const respuesta= await data.json();
+    const Search = await respuesta.Search;
+
+    console.log(Search);
+
+    if(Search!=null)
+    {   
+        document.getElementById("lista").innerHTML='';
+        Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                    <img width='100%' src=${p.Poster} alt="No hay poster"></img></div>`;
+        })
+
+    }
+
+}    
+
+     
+const buscar3=async(api_url)=>{
+
+    const respuesta= await axios(api_url);
+    const Search = await respuesta.data.Search;
+    console.log(respuesta.data);
+    
+    console.log(Search);
+
+    
+    if(Search!=null)
+    {
+        document.getElementById("lista").innerHTML='';
+        
+        Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                    <img width='100%' src=${p.Poster} alt="No hay poster"></img></div>`;
+        })
+
+    }
+
+}    
